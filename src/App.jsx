701a9a9ebc5e105facd9e28d9a355436291300f9
John <coldlombax@gmail.com>
2021-07-26 00:36:46 +0100

    Theme added with custom colors.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+
+import Navbar from './components/Navigation/Navbar';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#3F3D56',
+      light: '#575A89',
+    },
+    secondary: {
+      main: '#19CDFF',
+    },
+    success: {
+      main: '#7fff7c',
+    },
+  },
+  typography: {
+    fontFamily: "'Quicksand', sans-serif;",
+  },
+});
 
 export default function App() {
   return (
-    <div />
+    <ThemeProvider theme={theme}>
+      <Navbar />
+    </ThemeProvider>
+
   );
 }
